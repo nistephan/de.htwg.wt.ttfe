372096d25da2af9f7ee6653ee68c6a1db14c9e5e
nistephan <nicolai.stephan@gmx.net>
2018-12-30 18:51:23 +0100

    added toJson, ajax, keyboard input and move on server

--- app/controllers/TTFEController.scala
+++ app/controllers/TTFEController.scala
@@ -1,9 +1,13 @@
 package controllers
 
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes
 import javax.inject._
-
 import play.api.mvc._
 import de.htwg.se.ttfe.TTFE
+import de.htwg.se.ttfe.model.{Cell, Field}
+import play.api.libs.json._
+
+//case class Field(size: Int)
 
 @Singleton
 class TTFEController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
@@ -44,10 +48,104 @@ class TTFEController @Inject()(cc: ControllerComponents) extends AbstractControl
     Ok(views.html.ttfe(gameController, "About"))
   }
 
+  def move(direction: String) = Action {
+    gameController.moveDirection(direction)
+    Ok(views.html.ttfe(gameController, "move: " + direction))
+  }
+
+
   def gridToJson = Action {
-    //gameController.restart()
-    Ok(views.html.ttfe(gameController, "JSON"))
-    //Ok(gameController.toJson)
+    implicit val fieldWrites = new Writes[Field] {
+      def writes(field: Field) = Json.obj(
+        "size" ->field.grid.size,
+        "score" -> field.score,
+        "cells" -> Json.arr(
+          Json.obj(
+            "row" -> 0,
+            "col" -> 0,
+            "value" -> field.grid(0)(0).value
+          ),
+        Json.obj(
+          "row" -> 0,
+          "col" -> 1,
+          "value" -> field.grid(0)(1).value
+        ),
+          Json.obj(
+            "row" -> 0,
+            "col" -> 2,
+            "value" -> field.grid(0)(2).value
+          ),
+          Json.obj(
+            "row" -> 0,
+            "col" -> 3,
+            "value" -> field.grid(0)(3).value
+          ),
+          Json.obj(
+            "row" -> 1,
+            "col" -> 0,
+            "value" -> field.grid(1)(0).value
+          ),
+          Json.obj(
+            "row" -> 1,
+            "col" -> 1,
+            "value" -> field.grid(1)(1).value
+          ),
+          Json.obj(
+            "row" -> 1,
+            "col" -> 2,
+            "value" -> field.grid(1)(2).value
+          ),
+          Json.obj(
+            "row" -> 1,
+            "col" -> 3,
+            "value" -> field.grid(1)(3).value
+          ),
+          Json.obj(
+            "row" -> 2,
+            "col" -> 0,
+            "value" -> field.grid(2)(0).value
+          ),
+          Json.obj(
+            "row" -> 2,
+            "col" -> 1,
+            "value" -> field.grid(2)(1).value
+          ),
+          Json.obj(
+            "row" -> 2,
+            "col" -> 2,
+            "value" -> field.grid(2)(2).value
+          ),
+          Json.obj(
+            "row" -> 2,
+            "col" -> 3,
+            "value" -> field.grid(2)(3).value
+          ),
+          Json.obj(
+            "row" -> 3,
+            "col" -> 0,
+            "value" -> field.grid(3)(0).value
+          ),
+          Json.obj(
+            "row" -> 3,
+            "col" -> 1,
+            "value" -> field.grid(3)(1).value
+          ),
+          Json.obj(
+            "row" -> 3,
+            "col" -> 2,
+            "value" -> field.grid(3)(2).value
+          ),
+          Json.obj(
+            "row" -> 3,
+            "col" -> 3,
+            "value" -> field.grid(3)(3).value
+          ))
+      )
+    }
+
+    val json = Json.toJson(gameController.field)
+
+    Ok(json)
   }
 
 }
--- public/javascripts/ttfe.js
+++ public/javascripts/ttfe.js
@@ -1,40 +1,58 @@
-let size = 4
-
-let gameJson = {
-    size:4,
-    0: {0:0,1:1,2:0,3:0},
-    1: {0:1,1:0,2:0,3:0},
-    2: {0:1,1:0,2:0,3:0},
-    3: {0:1,1:0,2:0,3:0},
-};
-
 class Grid {
     constructor(size){
         this.size = size;
         this.cells = [];
     }
 
-
     fill(json) {
-       for (let x = 0; x < this.size; x++) {
-           for (let y = 0; y < this.size; y++) {
-               this.cells.push(json[x][y]);
-           }
+       for (let index = 0; index < this.size * this.size; index++) {
+               this.cells.push(json[index].value);
        }
     }
 }
 
-let grid = new Grid(gameJson.size)
-grid.fill(gameJson)
-
-function fillGrid(grid) {
+function updateGrid(grid, score) {
     for (let index = 0; index < grid.size * grid.size; index++) {
-            $("#scalar" + index).html(grid.cells[index]);
+            $("#cell" + index).html(grid.cells[index]);
     }
+    $("#score").html(score);
 }
 
+function moveOnServer(direction) {
+    $.get("/move/"+direction, function(data) {
+        console.log("Move on Server");
+    });
+}
+
+window.onkeyup = function(e) {
+   var key = e.keyCode ? e.keyCode : e.which;
+
+   if (key == 37) {
+       moveOnServer("L");
+   }else if (key == 38) {
+       moveOnServer("U");
+   }else if (key == 39) {
+       moveOnServer("R");
+   }else if (key == 40) {
+       moveOnServer("D");
+   }
+}
+
+function loadJson() {
+    $.ajax({
+        method: "GET",
+        url: "/json",
+        dataType: "json",
+
+        success: function (result) {
+            grid = new Grid(result.size);
+            grid.fill(result.cells);
+            updateGrid(grid, result.score);
+        }
+    });
+}
 
 $( document ).ready(function() {
     console.log( "Document is ready, filling grid" );
-    fillGrid(grid);
+    loadJson();
 }); 
